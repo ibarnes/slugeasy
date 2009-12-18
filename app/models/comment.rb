14b7eb7e619ea87d8d5e966fa7c5d1eeb3e39a3c
Ike <ikebarnes@beatcreators.com>
2009-12-18 00:37:43 -0500

    Finished the direct message functionality

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :message
   belongs_to :user
   attr_accessible :description
+
+  validates_length_of :description, :maximum => 160
+  validates_presence_of :description
 end
